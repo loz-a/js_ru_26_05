59e060c4124eeb0cc9e326a2665ea5e1ace06f20
Roman <kaboom.ua@gmail.com>
2016-06-19 14:35:23 -0700

    Update comments.js

--- src/AC/comments.js
+++ src/AC/comments.js
@@ -5,6 +5,7 @@ export function addComment(name, text, articleId) {
     return {
         type: ADD_COMMENT,
         payload: {
+            //это плохая практика - завязывать екшины на состояние стора
             id: newIntId('comments'),
             name,
             text,
