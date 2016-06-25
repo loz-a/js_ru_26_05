035a179eb3deb76891d0e3f91bbea9605e100625
loz <loz.andriy@gmail.com>
2016-06-25 18:46:54 +0300

    add redux-thunk

--- src/containers/ArticleList.js
+++ src/containers/ArticleList.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react'
 import ArticleList from '../components/ArticleList'
 import { connect } from 'react-redux'
 import { toArray } from '../store/utils'
+import { loadAllArticles } from '../AC/articles'
 
 // function ArticleListContainer({articles}) {
 //     return (
@@ -9,8 +10,21 @@ import { toArray } from '../store/utils'
 //     )
 // }
 
-const ArticleListContainer = ({articles}) => (<ArticleList articles = {articles} />)
+// const ArticleListContainer = ({articles}) => (<ArticleList articles = {articles} />)
+
+class ArticleListContainer extends React.Component {
+    componentDidMount() {
+        this.props.loadAllArticles()
+    }
+
+    render() {
+        return (
+            <ArticleList articles = {this.props.articles} />
+        )
+    }
+}
 
 export default connect(
-    (state) => ({articles: toArray(state.articles.toJS())})
+    (state) => ({articles: toArray(state.articles.toJS())}),
+    { loadAllArticles }
 )(ArticleListContainer)
--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,10 @@ import { createStore, applyMiddleware, compose} from 'redux'
 import reducer from '../reducer'
 import randomId from '../middlewares/randomId'
 import logger from '../middlewares/logger'
+import thunk from 'redux-thunk'
 
 const enhancer = compose(
-    applyMiddleware(randomId, logger),
+    applyMiddleware(thunk, randomId, logger),
     window.devToolsExtension ? window.devToolsExtension() : f => f
 )
 const store = createStore(reducer, {}, enhancer)
